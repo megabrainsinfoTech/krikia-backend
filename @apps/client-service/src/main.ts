61a6124fe3004d2ab961506129863eac91509196
Felix Chinonso Emmanuel <feelchi1star@gmail.com>
2024-03-08 18:19:13 +0100

    Updated ENV For Client Service

--- @apps/client-service/src/main.ts
+++ @apps/client-service/src/main.ts
@@ -7,12 +7,15 @@ import * as cookieParser from 'cookie-parser';
 void (async function () {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3040', 'http://localhost:3050'], // Allow requests from this origin
+    origin: [
+      process.env.CUSTOMER_FRONTEND_URL as string,
+      process.env.BUSINESS_FRONTEND_URL as string,
+    ], // Allow requests from this origin
     methods: 'GET,POST,PUT,PATCH,OPTION,DELETE', // Allow specific HTTP methods
     allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific request headers
     credentials: true, // Allow cookies to be sent with requests
     maxAge: 3600, // Set the CORS preflight cache duration
   });
   app.use(cookieParser(process.env.COOKIE_SECRET));
-  await app.listen(8001);
+  await app.listen(process.env.PORT as string);
 })();
